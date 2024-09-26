a4f5d937733584a43f1b71b11c8cdf00e882d4ca
ABDude123 <105649516+ABDude123@users.noreply.github.com>
2024-09-26 19:48:11 +0300

    Delete Gen-Rator directory

--- Gen-Rator/static/script.js
+++
@@ -1,8 +0,0 @@
-function generateCode() {
-    fetch('/generate')
-        .then(response => response.json())
-        .then(data => {
-            document.querySelector("#code span").innerText = data.code;
-            document.querySelector("#status span").innerText = data.status + " (API Status: " + data.api_status + ")";
-        });
-}
